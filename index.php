5b65df0acf690dbc0ff147af2992fc00f85d0b93
ryanwhitney <ryan.whitney@me.com>
2011-10-28 18:16:01 -0400

    Made the homepage valid html, added stylesheet

--- index.php
+++ index.php
@@ -1,10 +1,30 @@
-<form action="send.php"  method="POST">
-	Name: <input type="text" name="name[]" /> &nbsp;&nbsp;&nbsp; Number: <input type="text" name="number[]" /><br />
-	Name: <input type="text" name="name[]" /> &nbsp;&nbsp;&nbsp; Number: <input type="text" name="number[]" /><br />
-	Name: <input type="text" name="name[]" /> &nbsp;&nbsp;&nbsp; Number: <input type="text" name="number[]" /><br />
-	Name: <input type="text" name="name[]" /> &nbsp;&nbsp;&nbsp; Number: <input type="text" name="number[]" /><br />
-	<input type="submit">
-</form>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="author" content="Ryan Whitney" />
+	<title>Easily Share Phone Numbers Between a Group</title>
+	<meta name="description" content="Share phone numbers between group members in a snap. Just enter each member's name & number, click send, and everyone recieves the info!" />
+	<link rel="icon" type="image/png" href="/images/favicon.png" /> <!-- ADD THIS. -->
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js" type="text/javascript"></script>
+	<!--[if lt IE 9]>
+	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<form action="send.php"  method="POST">
+		<ul>
+			<li>Name: <input type="text" name="name[]" /> Number: <input type="text" name="number[]" /></li>
+			<li>Name: <input type="text" name="name[]" /> Number: <input type="text" name="number[]" /></li>
+			<li>Name: <input type="text" name="name[]" /> Number: <input type="text" name="number[]" /></li>
+			<li>Name: <input type="text" name="name[]" /> Number: <input type="text" name="number[]" /></li>
+		</ul>
+		<input type="submit">
+	</form>
+</body>
+</html>
+
 
 
 
